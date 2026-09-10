b4720792d163a5f18f9feff3359eeb35f3804d46
fatomkhalaf-creator <fatomkhalaf@gmail.com>
2026-09-11 00:35:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='alsour-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./icon.png','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
